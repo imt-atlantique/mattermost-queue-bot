1239e19229c9290536d9da59ef75fb9d138d60fb
Baptiste Gaultier <b.gaultier@gmail.com>
2020-09-24 12:11:17 +0200

    Create ticket-endpoint.py

---
+++ ticket-endpoint.py
@@ -0,0 +1,21 @@
+user_exists = False
+users = []
+with open('tickets', 'r') as f:
+    for line in f:
+        users.append(line)
+f.close()
+
+user = msg['user_id'] + ',' + msg['user_name'] + '\n'
+if user not in users:
+    users.append(user)
+    f = open('tickets', 'w')
+    for user in users:
+        f.write('%s' % user)
+    f.close()
+
+text = "Vous displosez du ticket n°";
+text += str(len(users));
+
+msg['payload'] = '{"response_type": "ephemeral", "text": "%s"}' % text;
+
+return msg
